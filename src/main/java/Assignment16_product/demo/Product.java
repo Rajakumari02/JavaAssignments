c4a2500114beb7710ed668dfb8228a389520562e
Rajakumari02 <146962118+Rajakumari02@users.noreply.github.com>
2025-09-22 10:43:04 +0530

    Add files via upload

---
+++ src/main/java/Assignment16_product/demo/Product.java
@@ -0,0 +1,49 @@
+package Assignment16_product.demo;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private Double price;
+    private Integer stock;
+    public Product(){
+    }
+
+    public Product(Integer id, String name, Double price, Integer stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
