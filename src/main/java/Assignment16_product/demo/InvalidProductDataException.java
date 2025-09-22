a3a9fce831f0d2bf9dbc6fda92dc4c35dfe7289e
Rajakumari02 <146962118+Rajakumari02@users.noreply.github.com>
2025-09-22 10:43:45 +0530

    Delete src/main/java directory

--- src/main/java/Assignment16_product/demo/InvalidProductDataException.java
+++
@@ -1,7 +0,0 @@
-package Assignment16_product.demo;
-
-public class InvalidProductDataException extends RuntimeException {
-    public InvalidProductDataException(String message) {
-        super(message);
-    }
-}
