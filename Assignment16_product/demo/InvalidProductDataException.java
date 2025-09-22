e225c845eaaaea7a3bde6cbe0e1921568e6705bb
Rajakumari02 <146962118+Rajakumari02@users.noreply.github.com>
2025-09-22 10:40:30 +0530

    Add files via upload

---
+++ Assignment16_product/demo/InvalidProductDataException.java
@@ -0,0 +1,7 @@
+package Assignment16_product.demo;
+
+public class InvalidProductDataException extends RuntimeException {
+    public InvalidProductDataException(String message) {
+        super(message);
+    }
+}
