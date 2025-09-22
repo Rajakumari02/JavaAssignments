52d6138200f2bc11912b481b4e28bf9cc5339ace
Rajakumari02 <146962118+Rajakumari02@users.noreply.github.com>
2025-09-22 10:42:21 +0530

    Delete Assignment16_product/demo directory

--- Assignment16_product/demo/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package Assignment16_product.demo;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
